a0e34cc097aceb1b1f2ee88d295e9636762c9bc5
Artem Solovev <85392692+TemaTerbi@users.noreply.github.com>
2022-10-10 22:39:21 +0300

    create tab bar with two view

---
+++ UnsplashAppIOS/MainPageModule/View/MainPageView.swift
@@ -0,0 +1,16 @@
+//
+//  MainPageView.swift
+//  UnsplashAppIOS
+//
+//  Created by Артем Соловьев on 10.10.2022.
+//
+
+import UIKit
+
+final class MainPageView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
--- UnsplashAppIOS/TabBarModule/View/TabBarView.swift
+++ UnsplashAppIOS/TabBarModule/View/TabBarView.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class TabBarView: UIViewController {
+final class TabBarView: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createTabBar()
     }
-
-
+    
+    private func createTabBar() {
+        let mainPage = MainPageView()
+        let favouritePage = FavouriteView()
+        
+        let mainVC = UINavigationController(rootViewController: mainPage)
+        let favouriteVC = UINavigationController(rootViewController: favouritePage)
+        
+        mainVC.title = "Главная страница"
+        favouriteVC.title = "Избранное"
+        
+        mainVC.tabBarItem.image = UIImage(systemName: "photo")
+        mainVC.tabBarItem.selectedImage = UIImage(systemName: "photo.fill")
+        favouriteVC.tabBarItem.image = UIImage(systemName: "suit.heart")
+        favouriteVC.tabBarItem.selectedImage = UIImage(systemName: "suit.heart.fill")
+        
+        tabBar.tintColor = .systemIndigo
+        tabBar.unselectedItemTintColor = .black
+        
+        let viewControllers = [
+            mainVC,
+            favouriteVC
+        ]
+        
+        tabBar.backgroundColor = .white
+        
+        self.setViewControllers(viewControllers, animated: true)
+    }
 }
 
