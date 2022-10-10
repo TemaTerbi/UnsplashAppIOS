351103174266cd6faf6f4ef532dd1f77d7f79724
Artem Solovev <85392692+TemaTerbi@users.noreply.github.com>
2022-10-10 22:21:21 +0300

    Initial Commit

---
+++ UnsplashAppIOS/TabBarModule/View/TabBarView.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarView.swift
+//  UnsplashAppIOS
+//
+//  Created by Артем Соловьев on 10.10.2022.
+//
+
+import UIKit
+
+class TabBarView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
